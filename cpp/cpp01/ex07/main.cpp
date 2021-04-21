73b6dfdd472090a189d29e47513c4b3c5fc3946f
Nirgoth <maklakovs.p@gmail.com>
2021-04-21 17:00:37 +0300

    554

--- cpp/cpp01/ex07/main.cpp
+++ cpp/cpp01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: whortenc <whortenc@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 16:37:59 by whortenc          #+#    #+#             */
-/*   Updated: 2021/04/21 16:38:27 by whortenc         ###   ########.fr       */
+/*   Updated: 2021/04/21 16:58:37 by whortenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,44 @@
 #include <sstream>
 #include <fstream>
 
-void ft_replace(std::string text, std::string s1, std::string s2, std::string file_name)
+void ft_replacer(std::string text, std::string s1, std::string s2, std::string file_name)
 {
+	std::ofstream	outputfile(file_name + ".output");
 	
-	std::ofstream	ofs(file_name + ".replace");
-
 	for(size_t i = 0; (i = text.find(s1, i)) != std::string::npos; i += s2.length())
-		text.output(i, s1.length(), s2);
-	ofs << text;
-	if (ofs.fail())
+		text.replace(i, s1.length(), s2);
+	outputfile << text;
+	if (outputfile.fail())
 		std::cerr << "\033[31m" << "Error type: " << "\033[0m" << std::strerror(errno) << std::endl;
+	//if(!outputfile.is_open())
+	//	std::cerr << "\033[31m" << "There is no rights to open output file" << "\033[0m" << std::endl;
 }
 
 int		main(int argc, char **argv)
 {
-	std::ifstream		ifs;
+	std::ifstream		openfile;
 	std::stringstream	ss;
 
 	if (argc != 4)
 	{
 		std::cerr << "\033[31m" << "Invalid number of arguments - should be strictly 4" << "\033[0m" << std::endl;
-		return (1);
+		return (0);
 	}
-	ifs.open(argv[1]);
-	if (ifs.fail())
-	{
-		std::cerr << "\033[31m" << "Open file error type: " << "\033[0m" << std::strerror(errno) << std::endl;
-		return (1);
-	}
+
 	if (!*argv[2] || !*argv[3])
 	{
 		std::cerr << "\033[31m" << "Empty strings are forbidden" << "\033[0m" << std::endl;
-		return (1);
+		return (0);
 	}
-	ss << ifs.rdbuf();
-	ft_replace(ss.str(), argv[2], argv[3], argv[1]);
-	return(0);
+	
+	openfile.open(argv[1]);
+	if (openfile.fail())
+	{
+		std::cerr << "\033[31m" << "Open file error type: " << "\033[0m" << std::strerror(errno) << std::endl;
+		return (0);
+	}
+
+	ss << openfile.rdbuf();
+	ft_replacer(ss.str(), argv[2], argv[3], argv[1]);
+	return(1);
 }
